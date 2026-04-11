a8c0264ae4187692618b03aef4fb6e5afb8d65d4
Thòng Hoài Quý <hoaiquy33@gmail.com>
2026-04-12 01:45:50 +0700

    fix AI logic

--- src/main/java/com/group18/chessgame/service/StockfishApiService.java
+++ src/main/java/com/group18/chessgame/service/StockfishApiService.java
@@ -13,18 +13,17 @@ public class StockfishApiService {
 
     public String getBestMoveFromApi(String fen, int depth) {
         try {
-            String clearFen = fen.trim();
-            String boardPart = clearFen.split(" ")[0];
-            String perfectFen = boardPart + " b - - 0 1";
-            String encodedFen = URLEncoder.encode(perfectFen, "UTF-8").replace("+", "%20");
+            String encodedFen = URLEncoder.encode(fen.trim(), "UTF-8").replace("+", "%20");
             String url = "https://stockfish.online/api/s/v2.php?fen=" + encodedFen + "&depth=" + depth;
 
             String response = restTemplate.getForObject(url, String.class);
-
             JsonNode root = objectMapper.readTree(response);
+            
             if (root.path("success").asBoolean()) {
                 String bestMoveLine = root.path("bestmove").asText();
-                return bestMoveLine.split(" ")[1];
+                if (bestMoveLine != null && bestMoveLine.contains(" ")) {
+                    return bestMoveLine.split(" ")[1];
+                }
             }
         } catch (Exception e) {
             System.err.println("Lỗi khi gọi API Stockfish: " + e.getMessage());
